1058e5d28d806b26313ecc32088bff3e3531a46e
DJ-root <zimbabvist@ukr.net>
2018-01-19 10:32:49 +0200

    Added new Unit4

---
+++ Edusharp.Unit4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edusharp.Unit4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
