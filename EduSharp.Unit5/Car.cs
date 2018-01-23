6bdf176fa6accc8252d8025b42654c86b71c6237
DJ-root <zimbabvist@ukr.net>
2018-01-23 11:49:14 +0200

    Started work with Classes

---
+++ EduSharp.Unit5/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EduSharp.Unit5
+{
+    public class Car
+    {
+        public string petName;
+        public int currSpeed;
+
+        public Car()
+        {
+            this.petName = "Chuck";
+            this.currSpeed = 10;
+        }
+
+        public Car(string pn)
+        {
+            this.petName = pn;
+        }
+
+        public Car(string petName, int currSpeed)
+        {
+            this.petName = petName;
+            this.currSpeed = currSpeed;
+        }
+
+        public void PrintState()
+        {
+            Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+        }
+
+        public void SpeedUp(int delta)
+        {
+            currSpeed += delta;
+        }
+    }
+}
---
+++ EduSharp.Unit5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduSharp.Unit5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Class Types *****\n");
+
+            Car myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+            Console.WriteLine();
+
+
+            Car chuck = new Car();
+            chuck.PrintState();
+
+            Car mary = new Car("Mary");
+            mary.PrintState();
+
+            Car daisy = new Car("Daisy", 75);
+            daisy.PrintState();
+            Console.WriteLine();
+
+
+           
+            Console.ReadLine();
+        }
+    }
+}
