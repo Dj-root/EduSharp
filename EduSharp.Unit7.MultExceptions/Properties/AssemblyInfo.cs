3cb29142207813292c682ce4b13b249da306bdd1
holms <holms.html@gmail.com>
2018-02-04 01:05:30 +0200

    Finished Unit7

---
+++ EduSharp.Unit7.MultExceptions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EduSharp.Unit7.MultExceptions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EduSharp.Unit7.MultExceptions")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c70d48b0-e68c-4899-b4c1-94fbba1d6e89")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EduSharp.Unit7.MultExceptions/Radio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EduSharp.Unit7.MultExceptions
+{
+    public class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            if (on)
+            {
+                Console.WriteLine("Jamming...");
+            }
+            else
+            {
+                Console.WriteLine("Quiet time...");
+            }
+        }
+    }
+}
