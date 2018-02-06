dfbfeb0ac854a09fe163ee69cb04c5fd4d39bea6
holms <holms.html@gmail.com>
2018-02-06 23:50:58 +0200

    Added IPointy

--- EduSharp.Unit8/Program.cs
+++ EduSharp.Unit8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace EduSharp.Unit8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** First Look at Interfaces *****");
+            string myStr = "Hello";
+            OperatingSystem unixOS = new OperatingSystem(PlatformID.Unix, new Version());
+            System.Data.SqlClient.SqlConnection sqlCnn = new SqlConnection();
+
+            CloneMe(myStr);
+            CloneMe(unixOS);
+            CloneMe(sqlCnn);
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void CloneMe(ICloneable c)
+        {
+            object theClone = c.Clone();
+            Console.WriteLine("Your clone is a: {0}", theClone.GetType().Name);
         }
     }
 }
