8ee4db1daae477a657bf87372390b4f22ce2ce82
holms <holms.html@gmail.com>
2018-02-20 23:40:28 +0200

    Added simple delegate creation

---
+++ EduSharp.Pluralsight.EventDelegate.DelegateAndEvents/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduSharp.Pluralsight.EventDelegate.DelegateAndEvents
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            DoWork(del1);
+
+            Console.Read();
+
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 called {0}", hours);
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed2 called {0}",hours);
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GoToMeetings);
+        }
+    }
+
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf, 
+        GenerateReports
+    }
+}
