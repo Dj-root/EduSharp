6b07942731e085562bd2a9f9268210d2916954a5
HoLmS <holms.html@gmail.com>
2018-03-01 17:59:12 +0200

    Finished Unit 11

--- Troelsen/EduSharp.Troelsen.Unit11/Program.cs
+++ Troelsen/EduSharp.Troelsen.Unit11/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -113,15 +114,47 @@ namespace EduSharp.Troelsen.Unit11
             Console.WriteLine("rect3 = {0}",rect3);
 
 
+            Console.WriteLine("***** Fun with Extension Methods *****\n");
+            int myInt = 123456789;
+            myInt.DisplayDefiningAssembly();
 
+            System.Data.DataSet d = new DataSet();
+            d.DisplayDefiningAssembly();
+
+            System.Media.SoundPlayer sp = new SoundPlayer();
+            sp.DisplayDefiningAssembly();
+
+            Console.WriteLine("Value of myInt: {0}", myInt);
+            Console.WriteLine("Reversed digits of myInt: {0}", myInt.ReverseDigits());
+
+
+            Console.WriteLine("\n***** Fun with Anonymous Types *****\n");
+            var myCar = new {Color = "Bright Pink", Make="Saab", CurrentSpeed=55};
+
+            Console.WriteLine("My car is a {0} {1}.", myCar.Color, myCar.Make);
+            BuildAnonType("BMW", "Black", 90);
+
+            ReflectOverAnonymousType(myCar);
 
             Console.ReadLine();
         }
 
 
+        static void ReflectOverAnonymousType(object obj)
+        {
+            Console.WriteLine("obj is an instance of: {0}", obj.GetType().Name);
+            Console.WriteLine("Base class of {0} is {1}", obj.GetType().Name, obj.GetType().BaseType);
+            Console.WriteLine("obj.ToString() == {0}", obj.ToString());
+            Console.WriteLine("obj.GetHashCode() == {0}", obj.GetHashCode());
+        }
 
+        static void BuildAnonType(string make, string color, int currSp)
+        {
+            var car = new {Make = make, Color = color, Speed = currSp};
+            Console.WriteLine("You have a {0} {1} going {2} MPH", car.Color, car.Make, car.Speed);
 
-
+            Console.WriteLine("ToString() == {0}", car.ToString());
+        }
 
         static void MultiIdexerWithDataTable()
         {
