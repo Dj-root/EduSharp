c15b2171b9695888a4a783e386cdabe8b181f0df
DJ-root <holms.html@gmail.com>
2018-03-15 00:09:27 +0200

    Tried some delegate examples from C# Nutshell

---
+++ Nutshell/EduSharp.Nutshell.Chapter4.Delegates/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduSharp.Nutshell.Chapter4.Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Testing invoking methods to delegate *****");
+            int[] values = {1, 2, 3};
+            Util.Transform(values, Square);
+            foreach (int i in values)
+            {
+                Console.Write(i + " ");
+            }
+
+            HardWorkDemo();
+            InstanceVsStatic();
+            DelegateVsInterface();
+
+
+
+
+            Console.ReadLine();
+        }
+
+        static int Square(int x) => x * x;
+
+        static void DelegateVsInterface() 
+        {
+            Console.WriteLine("\n***** Delegates VS Interfaces *****");
+
+            int[] values = { 1, 2, 3 };
+            Util.TransformAll(values, new Squarer());
+            foreach (int i in values)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+        static void InstanceVsStatic()
+        {
+            Console.WriteLine("\n***** Instance VS Static Delegates *****");
+
+            X x = new X();
+            ProgressReporter p = x.InstanceProgress;
+            p(99);
+            Console.WriteLine(p.Target);
+            Console.WriteLine(p.Method);
+        }
+
+        static void WriteProgressToConsole(int percentComplete) => Console.WriteLine(percentComplete);
+
+        static void WriteProgressToFile(int percentComplete) =>
+            System.IO.File.WriteAllText("progress.txt", percentComplete.ToString());
+
+        static void HardWorkDemo()
+        {
+            Console.WriteLine("***** Multicast Delegate *****");
+
+            ProgressReporter p = WriteProgressToConsole;
+            p += WriteProgressToFile;
+            Util.HardWork(p);
+        }
+
+    }
+}
---
+++ Nutshell/EduSharp.Nutshell.Chapter4.Delegates/Util.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduSharp.Nutshell.Chapter4.Delegates
+{
+    public delegate int Transformer(int x);
+    public delegate T Transformer<T>(T arg);
+
+    public delegate void ProgressReporter(int percentComplete);
+    class Util
+    {
+        public static void Transform<T>(T[] values, Transformer<T> t)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = t(values[i]);
+            }
+        }
+
+        public static void TransformAll(int[] values, ITransformer t)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = t.Transform(values[i]);
+            }
+        }
+
+//        public static void Transform(int[] values, Transformer t)
+//        {
+//            for (int i = 0; i < values.Length; i++)
+//            {
+//                values[i] = t(values[i]);
+//            }
+//        }
+
+        public static void HardWork(ProgressReporter p)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                p(i * 10);
+                System.Threading.Thread.Sleep(100);
+            }
+            
+        }
+    }
+}
